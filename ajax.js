fad02fcbe1be0eef3f0d68dc80261f926d06dd2a
deepmeena70 <deepmeena70@gmail.com>
2020-05-18 20:38:33 +0530

    ajax edit and update functionality updated

--- ajax.js
+++ ajax.js
@@ -38,12 +38,35 @@ function edit(val) {
     xhr.send();
 }
 
+function update() {
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("main").innerHTML =
+                this.responseText;
+        }
+    }
+
+    var id = document.getElementById("id").value;
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    console.log(id);
+    console.log(username);
+    console.log(email);
+    console.log(password);
+
+    xhr.open("GET", "update.php/?id=" + id +
+        "&username=" + username + " &email= " + email + " &password=" + password, true);
+    // console.log("check");
+    xhr.send();
+}
 
 
-// document.getElementById("update").addEventListener("submit", function(event) {
+// document.getElementById("update-data").addEventListener("submit", function(event) {
 //     event.preventDefault();
 
-//     console.log("check");
+//     console.log("check!");
 
 //     xhr.onreadystatechange = function() {
 //         if (this.readyState == 4 && this.status == 200) {
--- views/edit_users.php
+++ views/edit_users.php
@@ -1,6 +1,6 @@
 <?php foreach($result as $key=>$val): ?>
 <tr>
-    <form id="update">
+    <form id="update-data" >
         <th scope="row">
             <?php echo $val[0]?>
         </th>
@@ -11,16 +11,18 @@
             <input id="username" type="text" name="username" value="<?php echo $val[1] ?>"> 
         </td>
         <td>
-            <input id="email" type="email" name="email" value="<?php echo$val[2] ?>"> 
+            <input id="email" type="email" name="email" value="<?php echo $val[2] ?>"> 
         </td> 
         <td>              
-            <input id="password" type="password" name="password" value="<?php echo$val[3] ?>"> 
+            <input id="password" type="password" name="password" value="<?php echo $val[3] ?>"> 
         </td>
         <td>
-            <input class="btn btn-primary" type="submit" value="Update" >               
+            <input id="submit" class="btn btn-primary" type="submit" value="Update" onclick="update()">               
         </td>
     </form>
 </tr>
 <?php endforeach?>
+
+
 
 
